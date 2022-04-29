137548ee26b069fb9b15ef8ecdc6f530519bd4c3
narfman14 <crknapp@stafast.com>
2022-04-29 00:43:00 -0400

    fixed loop crash when factionlist is empty

--- gangPrep.js
+++ gangPrep.js
@@ -2,30 +2,36 @@
 export async function main(ns) {
 	const gangName = "Slum Snakes"
 	const crime = "Homicide";
+
 	ns.disableLog("sleep");
 	ns.tail();
-	
+
 	while (!ns.gang.inGang()) {
-		ns.clearLog();
 		let factionList = ns.checkFactionInvitations();
 		let karma = ns.heart.break();
 		ns.print("Current Karma : " + karma);
-		//Build Karma
 		if (karma > -55000) {
+			ns.commitCrime(crime);
+			await ns.sleep(3000);
 			for (let faction of factionList) {
 				if (faction == gangName) {
 					ns.joinFaction(faction);
+					ns.toast("Joined Slum Snakes", "success", 3000)
 				}
 				else {
-					ns.commitCrime(crime);
-					await ns.sleep(3000);
+					ns.print("Slum Snakes is not available")
 				}
 			}
 		}
-		else if ((ns.getServerMaxRam("home")-ns.getServerUsedRam("home")) >= 32){
+		else if ((ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) >= 32) {
 			ns.gang.createGang(gangName);
-			ns.exec("gang-CK.js","home");
+			ns.toast("Created Gang", "success", 3000)
+			ns.exec("gang-CK.js", "home");
+			await ns.sleep(100);
 		}
-		else{ns.print("Not enough available RAM");}
+		else {
+			ns.print("Not enough available RAM");
+			await ns.sleep(100);
+		}
 	}
 }
